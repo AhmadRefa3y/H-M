ab540986df9ccce5b806b31713b2f2ded32c060c
AhmedOmar <ahmedrefay3000@gmail.com>
2024-10-10 16:39:31 +0300

    Pagination

--- src/components/ProductsListPage/ProductsContainer.tsx
+++ src/components/ProductsListPage/ProductsContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from './ProductCard'
 
 const ProductsContainer = ({
@@ -10,17 +10,35 @@ const ProductsContainer = ({
         price: number
     }[]
 }) => {
+    const [prdouctsNumber, setprdouctsNumber] = useState(10)
+
+    const loadMore = () => {
+        setprdouctsNumber(prdouctsNumber + 10)
+    }
+
     return (
-        <div className=" flex-wrap flex justify-between md:justify-start mt-5">
-            {products.map((product, index) => (
-                <Product
-                    img={product.image}
-                    name={product.name}
-                    price={product.price}
-                    key={index}
-                />
-            ))}
-        </div>
+        <>
+            <div className=" flex-wrap flex justify-between md:justify-start mt-5">
+                {products.slice(0, prdouctsNumber).map((product, index) => (
+                    <Product
+                        img={product.image}
+                        name={product.name}
+                        price={product.price}
+                        key={index}
+                    />
+                ))}
+            </div>
+            {products.length > prdouctsNumber && (
+                <div className="w-full items-center justify-center flex">
+                    <button
+                        onClick={loadMore}
+                        className="w-fit mx-auto text-center  text-white bg-black  px-10 py-1"
+                    >
+                        تحميل المزيد من المنتجات
+                    </button>
+                </div>
+            )}
+        </>
     )
 }
 
